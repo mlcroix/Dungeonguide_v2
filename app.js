d830d494b8f7eb1bbecd99bcbbf8cf7a0c208480
Michel du Croix <mlcroix@avans.nl>
2018-10-30 20:19:50 +0100

    cors fix

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000;
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
 server.listen(port, function() {
     console.log('Gulp is starting my app on PORT: ' + port)
 });
